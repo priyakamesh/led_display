44d84a2a27d5ceec4b7abdfefbbb88226291a1f9
krishnapriya <krishnapriyakamesh@gmail.com>
2017-05-03 14:40:42 -0500

    added text to ascii and ascii to text conversion

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
+
+//initializing an array
 var array = [];
+
+
 function getInput() {
    var input = document.getElementById('input').value.toUpperCase();
-   var canvas = document.getElementById("myCanvas");
+   // var output = document.getElementById("output")
+    // output.value = "";
+    var output = '';
+    for (var i = 0; i < input.length; i++) {
+
+      output += input[i].charCodeAt(0) + " ";
+
+   }
+   array = output.split(" ")
+   array.pop()
+   console.log("array",array);
+   var result = array.map((num) =>{
+    if(num > 31) {
+      return String.fromCharCode(parseInt(num))
+    }
+
+   })
+   document.getElementById("output").innerHTML = result.join("")
+
+
+
+
+// draw letters using canvas, now only A letter is drawn.
+  var canvas = document.getElementById("myCanvas");
    var ctx = canvas.getContext("2d");
    ctx.fillStyle = "white";
    ctx.fillRect(0, 0, 1200, 160);
@@ -25,7 +52,7 @@ function getInput() {
       ctx.stroke();
     }
     ctx.fillStyle = 'blue';
-    if (input.charAt(0) === "A") {
+
        ctx.fillRect(0,0,10,10)
        ctx.fillRect(10,0,10,10)
        ctx.fillRect(20,0,10,10)
@@ -37,35 +64,6 @@ function getInput() {
        ctx.fillRect(20,30,10,10)
        ctx.fillRect(10,20,10,10)
 
-   }
-   else{
-      alert("Now only letter 'a' will work, working on other alphabets")
-   }
    var imgData = ctx.getImageData(0,0,30,40);
    console.log("imgData",imgData);
-   var dataUrl = canvas.toDataUrl(imgData.ImageData.data)
-   console.log("dataUrl",dataUrl);
 }
-
-
-
-
-// function U2A() {
-//    var str = document.getElementById("input").value
-//    console.log("str",str);
-//     var reserved = '';
-//     var regexp = \p{L}\p{M}*
-//     var code = str.match(regexp);
-
-//     if (code === null) {
-//         document.getElementById("output").innerHTML = str;
-
-//     }
-//     else {
-//     for (var i = 0; i < code.length; i++) {
-//         reserved += String.fromCharCode(code[i].replace(regexp, ''));
-//         console.log("reserved",reserved);
-//     }
-//      document.getElementById("output").innerHTML = reserved;
-//   }
-// }
